de3823b8ab6281926d9a4727ee24abf2813e291b
luguangdong <lgd15095370993@gmail.com>
2023-10-08 18:25:09 +0800

    init boot.

---
+++ ums-boot/ums-boot-service/src/main/java/com/lucifer/cloud/boot/ums/UmsServerApplication.java
@@ -0,0 +1,15 @@
+package com.lucifer.cloud.boot.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lucifer
+ * @date 2023/10/8 17:49
+ */
+@SpringBootApplication
+public class UmsServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UmsServerApplication.class, args);
+    }
+}
---
+++ ums-boot/ums-boot-service/src/main/java/com/lucifer/cloud/boot/ums/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.lucifer.cloud.boot.ums.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lucifer
+ * @date 2023/10/8 18:02
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test01")
+    @PreAuthorize("hasAuthority('SCOPE_message.read')")
+    public String test01() {
+        return "test01";
+    }
+
+    @GetMapping("/test02")
+    @PreAuthorize("hasAuthority('SCOPE_message.write')")
+    public String test02() {
+        return "test02";
+    }
+
+    @GetMapping("/app")
+    @PreAuthorize("hasAuthority('app')")
+    public String app() {
+        return "app";
+    }
+
+}
