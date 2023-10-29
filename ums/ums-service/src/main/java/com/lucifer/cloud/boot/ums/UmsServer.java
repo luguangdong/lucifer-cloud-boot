33240340f3cdfa3e8befc5af2b915cbbfa12636b
luguangdong <lgd15095370993@gmail.com>
2023-10-29 15:59:19 +0800

    init boot.

--- ums/ums-service/src/main/java/com/lucifer/cloud/boot/ums/UmsServer.java
+++ ums/ums-service/src/main/java/com/lucifer/cloud/boot/ums/UmsServer.java
@@ -2,11 +2,13 @@ package com.lucifer.cloud.boot.ums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author lucifer
  * @date 2023/10/8 17:49
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UmsServer {
     public static void main(String[] args) {
