a409657d56a207e3fecc200a55b8fe45f96cf796
luguangdong <lgd15095370993@gmail.com>
2023-11-16 11:09:23 +0800

    init boot.

--- ums/ums-service/src/main/java/com/lucifer/cloud/boot/ums/config/SecurityConfig.java
+++ ums/ums-service/src/main/java/com/lucifer/cloud/boot/ums/config/SecurityConfig.java
@@ -9,15 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.web.filter.CorsFilter;
 
 /**
- *  客户端一些安全配置
- *  配置客户端跨域访问
- *  配置客户端不需要认证的放行路径
+ * 客户端一些安全配置
+ * 配置客户端跨域访问
+ * 配置客户端不需要认证的放行路径
  */
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
     private final CorsFilter corsFilter;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(HttpSecurity http) {
         http.addFilter(corsFilter);
