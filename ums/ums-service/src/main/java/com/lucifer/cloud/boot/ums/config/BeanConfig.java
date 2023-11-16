ead511bf2facc56000feaa5706435bb0e05c164f
luguangdong <lgd15095370993@gmail.com>
2023-11-16 10:27:09 +0800

    init boot.

---
+++ ums/ums-service/src/main/java/com/lucifer/cloud/boot/ums/config/BeanConfig.java
@@ -0,0 +1,57 @@
+package com.lucifer.cloud.boot.ums.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 将bean注入至ioc的配置类
+ *
+ * @author lucifer
+ */
+@Configuration
+@EnableConfigurationProperties
+public class BeanConfig {
+
+    /**
+     * 跨域过滤器配置
+     *
+     * @return CorsFilter
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+
+        // 初始化cors配置对象
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 设置允许跨域的域名,如果允许携带cookie的话,路径就不能写*号, *表示所有的域名都可以跨域访问
+        configuration.addAllowedOrigin("http://lucifer-cloud.com");
+        configuration.addAllowedOrigin("http://lucifer-cloud.com:5173");
+        configuration.addAllowedOrigin("http://127.0.0.1");
+        configuration.addAllowedOrigin("http://127.0.0.1:5173");
+        configuration.addAllowedOrigin("http://10.210.209.69");
+        configuration.addAllowedOrigin("http://10.210.209.69:5173");
+        // 设置跨域访问可以携带cookie
+        configuration.setAllowCredentials(true);
+        // 允许所有的请求方法 ==> GET POST PUT Delete
+        configuration.addAllowedMethod("*");
+        // 允许携带任何头信息
+        configuration.addAllowedHeader("*");
+
+        // 初始化cors配置源对象
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+
+        // 给配置源对象设置过滤的参数
+        // 参数一: 过滤的路径 == > 所有的路径都要求校验是否跨域
+        // 参数二: 配置类
+        configurationSource.registerCorsConfiguration("/**", configuration);
+
+        // 返回配置好的过滤器
+        return new CorsFilter(configurationSource);
+    }
+
+
+}
