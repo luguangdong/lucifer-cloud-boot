14057c550ce074e68d72a92df0f6774373468691
luguangdong <lgd15095370993@gmail.com>
2023-12-06 13:14:31 +0800

    init boot.

--- deploy/ums/gitee/start.sh
+++ deploy/ums/gitee/start.sh
@@ -8,9 +8,9 @@ if [ -n "$pid" ]
 then
 	kill -9 $pid
 	echo "关闭服务 $JARFILE 成功"
-	source /etc/profile
-  nohup java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profiles.active=$ENV > log &
-  pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
-  echo "新服务进程pid=$pid"
-  echo "启动服务 $JARFILE 成功"
 fi
+source /etc/profile
+nohup java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profiles.active=$ENV > log &
+pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
+echo "新服务进程pid=$pid"
+echo "启动服务 $JARFILE 成功"
