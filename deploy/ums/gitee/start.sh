8cd6920a481aceebe8343ea28268b980592bb1d9
luguangdong <lgd15095370993@gmail.com>
2023-12-08 18:25:32 +0800

    init boot.

--- deploy/ums/gitee/start.sh
+++ deploy/ums/gitee/start.sh
@@ -2,7 +2,7 @@
 FILEPATH=/data/deploy/lucifer-cloud/ums
 JARFILE=ums.jar
 ENV=tencent
-JAVA_HOME = /data/java/jdk-17.0.8/bin
+JAVA_HOME=/data/java/jdk-17.0.8/bin
 pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
 echo "旧服务进程pid=$pid"
 if [ -n "$pid" ]
@@ -11,7 +11,7 @@ then
 	echo "关闭服务 $JARFILE 成功"
 fi
 source /etc/profile
-nohup $JARFILE/java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profiles.active=$ENV > log &
+nohup JAVA_HOME/java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profiles.active=$ENV > log &
 pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
 echo "新服务进程pid=$pid"
 echo "启动服务 $JARFILE 成功"
