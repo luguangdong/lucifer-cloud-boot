f2e944d46a2ef5608d55015aaff94a925cefdba4
luguangdong <lgd15095370993@gmail.com>
2023-12-31 16:38:02 +0800

    optimize.

---
+++ blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/config/BlogConstant.java
@@ -0,0 +1,10 @@
+package com.lucifer.cloud.boot.blog.config;
+
+/**
+ * @author lucifer
+ * @date 2023/12/31 14:37
+ */
+public interface BlogConstant {
+
+    public  static final String STAR = "1";
+}
--- blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/service/impl/StarServiceImpl.java
+++ blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/service/impl/StarServiceImpl.java
@@ -1,12 +1,13 @@
 package com.lucifer.cloud.boot.blog.service.impl;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lucifer.cloud.boot.blog.config.BlogConstant;
 import com.lucifer.cloud.boot.blog.config.UserSystem;
 import com.lucifer.cloud.boot.blog.domin.bo.Exhibition;
 import com.lucifer.cloud.boot.blog.domin.bo.Star;
 import com.lucifer.cloud.boot.blog.domin.dto.star.StarConverter;
+import com.lucifer.cloud.boot.blog.mapper.ExhibitionMapper;
 import com.lucifer.cloud.boot.blog.mapper.StarMapper;
-import com.lucifer.cloud.boot.blog.service.ExhibitionService;
 import com.lucifer.cloud.boot.blog.service.StarService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,22 +25,26 @@ public class StarServiceImpl extends ServiceImpl<StarMapper, Star> implements St
     @Resource
     private UserSystem userSystem;
 
-//    @Resource
-//    private ExhibitionService exhibitionService;
+    @Resource
+    private ExhibitionMapper exhibitionMapper;
 
     @Override
     public Boolean star(HttpServletRequest request, String uid, String star_type, String type) {
         Long userId = userSystem.userId(request);
+
+        Boolean starRes = Boolean.FALSE;
+
         Star sr = getOne(Wrappers.lambdaQuery(Star.class).eq(Star::getUser_id, userId).eq(Star::getStar_id, uid));
         if (Objects.isNull(sr)){
             Star star = StarConverter.converterReq2Star(userId,uid, star_type, type);
-            boolean save = save(star);
-//            if(save){
-//                exhibitionService.update(Wrappers.lambdaUpdate(Exhibition.class).setSql("`thumbs_up`=`thumbs_up`+1").eq(Exhibition::getUid,uid).eq(Exhibition::getUser_id,userId));
-//            }
-            return save;
+            starRes = save(star);
         }else {
-            return update(Wrappers.lambdaUpdate(Star.class).set(Star::getStar_type,star_type).eq(Star::getUser_id,userId).eq(Star::getStar_id,uid).eq(Star::getType,type));
+            starRes = update(Wrappers.lambdaUpdate(Star.class).set(Star::getStar_type,star_type).eq(Star::getUser_id,userId).eq(Star::getStar_id,uid).eq(Star::getType,type));
         }
+        exhibitionMapper.update(new Exhibition(),Wrappers.lambdaUpdate(Exhibition.class)
+                .setSql(BlogConstant.STAR.equals(star_type) ?  "`thumbs_up`=`thumbs_up`+1":"`thumbs_up`=`thumbs_up`-1").eq(Exhibition::getUid,uid)
+                .eq(Exhibition::getUser_id,userId));
+
+        return starRes;
     }
 }
