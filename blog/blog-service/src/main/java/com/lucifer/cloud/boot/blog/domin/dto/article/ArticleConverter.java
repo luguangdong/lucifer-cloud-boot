5769a12ca88a9769a56d7cbc4566d718737b7ec0
luguangdong <lgd15095370993@gmail.com>
2024-01-12 17:54:17 +0800

    optimize.

--- blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/domin/dto/article/ArticleConverter.java
+++ blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/domin/dto/article/ArticleConverter.java
@@ -1,8 +1,14 @@
 package com.lucifer.cloud.boot.blog.domin.dto.article;
 
+import com.alibaba.nacos.shaded.com.google.common.collect.Lists;
 import com.lucifer.cloud.boot.blog.domin.bo.Article;
+import com.lucifer.cloud.boot.blog.domin.dto.user.UserInfo;
 import com.lucifer.cloud.boot.blog.util.GenerateUtils;
 import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author lucifer
@@ -16,5 +22,19 @@ public class ArticleConverter {
         BeanUtils.copyProperties(articleReq,article);
         article.setUser_id(userId);
         return article;
+    }
+
+
+    public static ArticleDto converter2Dto(List<Article> articleList, UserInfo user_info,Long userId){
+        List<ArticleInfo> article_info = Optional.ofNullable(articleList).orElse(Lists.newArrayList())
+                .stream().map(article -> {
+                    ArticleInfo articleInfo = new ArticleInfo();
+                    BeanUtils.copyProperties(article,articleInfo);
+                    articleInfo.setUserInfo(user_info);
+                    articleInfo.setUser_id(String.valueOf(userId));
+                    return articleInfo;
+                }).collect(Collectors.toList());
+        CardInfo cardInfo = new CardInfo();
+        return ArticleDto.builder().article_info(article_info).card_info(cardInfo).count(articleList.stream().count()).build();
     }
 }
--- blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/service/impl/ArticleServiceImpl.java
+++ blog/blog-service/src/main/java/com/lucifer/cloud/boot/blog/service/impl/ArticleServiceImpl.java
@@ -1,15 +1,26 @@
 package com.lucifer.cloud.boot.blog.service.impl;
+import com.alibaba.nacos.shaded.com.google.common.collect.Lists;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lucifer.cloud.boot.blog.config.UserSystem;
 import com.lucifer.cloud.boot.blog.domin.bo.Article;
+import com.lucifer.cloud.boot.blog.domin.bo.User;
 import com.lucifer.cloud.boot.blog.domin.dto.article.ArticleConverter;
 import com.lucifer.cloud.boot.blog.domin.dto.article.ArticleDto;
 import com.lucifer.cloud.boot.blog.domin.dto.article.ArticleReq;
+import com.lucifer.cloud.boot.blog.domin.dto.user.UserConverter;
+import com.lucifer.cloud.boot.blog.domin.dto.user.UserInfo;
 import com.lucifer.cloud.boot.blog.mapper.ArticleMapper;
+import com.lucifer.cloud.boot.blog.mapper.UserMapper;
 import com.lucifer.cloud.boot.blog.service.ArticleService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author lucifer
@@ -21,9 +32,34 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
     @Resource
     private UserSystem userSystem;
 
+    @Resource
+    private UserMapper userMapper;
+
     @Override
     public ArticleDto info(HttpServletRequest request, Integer page, Integer limit, Integer type, String sort) {
-        return ArticleDto.builder().build();
+
+        Long userId = userSystem.userId(request);
+        User user = userMapper.selectById(userId);
+        UserInfo user_info = UserConverter.convertInfo(user);
+
+        List<Article> articleList = Lists.newArrayList();
+        Long count = 0L;
+        if(Objects.isNull(page) && Objects.isNull(limit)){
+            articleList = list(Wrappers.lambdaQuery(Article.class)
+                    .eq(Article::getUser_id, userId)
+                    .last(StringUtils.isNotBlank(sort), "order by " + sort));
+            count = Long.valueOf(articleList.size());
+        }else {
+            Page<Article> rowPage = new Page<>(page, limit);
+            Page<Article> articlePage = this.baseMapper.selectPage(rowPage,Wrappers.lambdaQuery(Article.class)
+                    .eq(Article::getUser_id,userId)
+                    .last(StringUtils.isNotBlank(sort),"order by "+ sort)
+            );
+            articleList = articlePage.getRecords();
+            count = articlePage.getTotal();
+        }
+
+        return ArticleConverter.converter2Dto(articleList,user_info,userId);
     }
 
     @Override
